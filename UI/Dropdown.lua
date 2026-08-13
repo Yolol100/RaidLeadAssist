087179bd1e6005dafad04b5a3469651298391291
Yolol100 <Andrew_Baeten@hotmail.com>
2026-08-13 15:52:19 +0200

    Reuse dropdown option frames

--- UI/Dropdown.lua
+++ UI/Dropdown.lua
@@ -5,6 +5,34 @@ local Theme = ns:GetModule("UI.Theme")
 local Dropdown = {}
 Dropdown.__index = Dropdown
 
+local function ensureOptionButton(instance, index)
+    local button = instance.options[index]
+    if button then return button end
+
+    button = CreateFrame("Button", nil, instance.menu)
+    button:SetHeight(28)
+
+    button.text = button:CreateFontString(nil, "OVERLAY")
+    button.text:SetFont(Theme.font, 11, "OUTLINE")
+    button.text:SetPoint("LEFT", 8, 0)
+    button.text:SetTextColor(0.91, 0.94, 0.91, 1)
+
+    button.highlight = button:CreateTexture(nil, "BACKGROUND")
+    button.highlight:SetAllPoints()
+    button.highlight:SetColorTexture(0.33, 0.45, 0.17, 0.20)
+    button.highlight:Hide()
+
+    button:SetScript("OnEnter", function(self) self.highlight:Show() end)
+    button:SetScript("OnLeave", function(self) self.highlight:Hide() end)
+    button:SetScript("OnClick", function(self)
+        instance.menu:Hide()
+        if instance.onSelect and self.optionKey then instance.onSelect(self.optionKey) end
+    end)
+
+    instance.options[index] = button
+    return button
+end
+
 function Dropdown:Create(parent)
     local instance = setmetatable({}, Dropdown)
 
@@ -42,44 +70,30 @@ function Dropdown:Create(parent)
     instance.frame = button
     instance.menu = menu
     instance.options = {}
+    instance.onSelect = nil
     return instance
 end
 
 function Dropdown:SetOptions(options, selectedKey, onSelect)
-    for _, optionButton in ipairs(self.options) do optionButton:Hide() end
-    table.wipe(self.options)
+    self.onSelect = onSelect
 
-    local height = 6
     for index = 1, #options do
         local option = options[index]
-        local button = CreateFrame("Button", nil, self.menu)
-        button:SetHeight(28)
+        local button = ensureOptionButton(self, index)
+        button:ClearAllPoints()
         button:SetPoint("TOPLEFT", 4, -4 - (index - 1) * 28)
         button:SetPoint("TOPRIGHT", -4, -4 - (index - 1) * 28)
-
-        button.text = button:CreateFontString(nil, "OVERLAY")
-        button.text:SetFont(Theme.font, 11, "OUTLINE")
-        button.text:SetPoint("LEFT", 8, 0)
+        button.optionKey = option.key
         button.text:SetText(option.name)
-        button.text:SetTextColor(0.91, 0.94, 0.91, 1)
-
-        button.highlight = button:CreateTexture(nil, "BACKGROUND")
-        button.highlight:SetAllPoints()
-        button.highlight:SetColorTexture(0.33, 0.45, 0.17, 0.20)
-        button.highlight:Hide()
-
-        button:SetScript("OnEnter", function() button.highlight:Show() end)
-        button:SetScript("OnLeave", function() button.highlight:Hide() end)
-        button:SetScript("OnClick", function()
-            self.menu:Hide()
-            if onSelect then onSelect(option.key) end
-        end)
-
-        self.options[#self.options + 1] = button
-        height = height + 28
+        button:Show()
     end
 
-    self.menu:SetHeight(height + 4)
+    for index = #options + 1, #self.options do
+        self.options[index].optionKey = nil
+        self.options[index]:Hide()
+    end
+
+    self.menu:SetHeight(10 + (#options * 28))
     self:SetSelected(selectedKey, options)
 end
 
