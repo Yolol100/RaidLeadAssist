41b1a48aabc34ed8d189cc94fdabac53a19c8237
Yolol100 <Andrew_Baeten@hotmail.com>
2026-08-18 00:36:46 +0200

    Preserve established Nekzali raid warning wording

--- Encounters/VenomousAbyss/Nekzali.lua
+++ Encounters/VenomousAbyss/Nekzali.lua
@@ -7,7 +7,7 @@ local function baseCalls(pyreAction, flameAction)
             key = "adds",
             ability = "Restless Amani",
             action = "Kill adds",
-            warning = "KILL ADDS",
+            warning = "KILL ADS",
             voice = "Adds",
             spellIDs = { 1295397, 1297630 },
             prepareSeconds = 7,
