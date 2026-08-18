79dce61a680a1c295dcbbe465565c385e1a73de4
Yolol100 <Andrew_Baeten@hotmail.com>
2026-08-18 13:59:21 +0200

    Scope assignment binding test to Twin Fangs override

--- tests/test_assignment_call_bindings.lua
+++ tests/test_assignment_call_bindings.lua
@@ -10,7 +10,6 @@ for _, file in ipairs({"CoiledAltar.lua","Explorers.lua","Nekzali.lua","Sentinel
     T.Load("Encounters/VenomousAbyss/" .. file, ns)
 end
 T.Load("Encounters/AssignmentRegistry.lua", ns)
-T.Load("Encounters/SszorakAssignmentOverride.lua", ns)
 T.Load("Encounters/TwinFangsAssignmentOverride.lua", ns)
 
 local Registry = ns:GetModule("Encounters.Registry")
